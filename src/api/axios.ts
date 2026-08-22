4911e14cfe41ee0752ac8985806e747e0a1fe517
HETARTH <hetarth@jarvistechnolabs.com>
2026-08-22 19:53:32 +0530

    Change API URL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -17,8 +17,8 @@ import {
 } from '../types';
 import { clearAuthSession, getAuthToken } from '../utils/authSession';
 
-// const BASE_URL = 'https://expense-tracker-backend-yj1t.onrender.com/api';
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://expense-tracker-backend-yj1t.onrender.com/api';
+// const BASE_URL = 'http://localhost:5000/api';
 
 const api: AxiosInstance = axios.create({
   baseURL: BASE_URL,
